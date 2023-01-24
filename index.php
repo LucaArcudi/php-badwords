5c49971474b7498b83ca057cace27b55e24e1c4c
LucaArcudi <arcudilu@gmail.com>
2023-01-24 21:53:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <form action="./controller.php" method="GET">
+            <textarea name="content" dirname="explanation.dir" rows="5" cols="100">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas, voluptatum.
+            </textarea>
+            <input type="text" name="badword">
+            <button type="submit">INVIA</button>
+        </form>
+    </main>
+</body>
+
+</html>
