00cca06d6c08ba321205c1161bf266378070c9f0
LucaArcudi <arcudilu@gmail.com>
2023-01-24 23:30:40 +0100

    replacing userBadword

--- controller.php
+++ controller.php
@@ -10,18 +10,50 @@
 
 <body>
     <main>
-        <p>
-            <?php
-            $textContent = $_GET["content"];
-            echo $textContent;
-            ?>
-        </p>
 
-        <span>
-            <?php
-            echo strlen($textContent);
-            ?>
-        </span>
+        <?php
+        $textContent = $_GET["content"];
+        $userBadword = $_GET["badword"]
+        ?>
+
+        <div>
+
+            <h2>PARAGRAFO</h2>
+
+            <p>
+                <?php
+                echo $textContent;
+                ?>
+            </p>
+
+            <span>
+                <?php
+                echo "lunghezza: " . strlen($textContent);
+                ?>
+            </span>
+        </div>
+
+        <div>
+
+            <h2>NUOVO PARAGRAFO</h2>
+
+            <p>
+                <?php
+                if (str_contains($textContent, $userBadword)) {
+                    $textContent = str_ireplace($userBadword, "***", $textContent);
+                    echo $textContent;
+                } else {
+                    echo $textContent;
+                }
+                ?>
+            </p>
+
+            <span>
+                <?php
+                echo "lunghezza: " . strlen($textContent);
+                ?>
+            </span>
+        </div>
     </main>
 </body>
 
